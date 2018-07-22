f1aaf96668e5d3641e1b4b8e02d79258216b98fd
Mike A. Trethewey <miketrethewey@gmail.com>
2018-07-21 23:38:28 -0700

    Add new M1 Kraid locations

--- script/classes/Region/Metroid/Kraid/KraidMain.js
+++ script/classes/Region/Metroid/Kraid/KraidMain.js
@@ -3,6 +3,8 @@ class KraidMain extends Kraid {
 	super(name,subname);
 	let regionName = name + subname;
 	this.locations = new LocationCollection([
+		new Location("","Upper Right Area Top Floor",171,360,regionName),
+		new Location("","Upper Right Area Middle Floor",171,381,regionName),
 		new Location("","Left of First Doors",66,444,regionName),										// 0x1504
 		new Location("","Right of First Doors",171,444,regionName),										// 0x1509
 		new Location("","Right of Second Doors",192,465,regionName),									// 0x160A
